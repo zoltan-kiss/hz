e6baf52a1a35d4d28666bb558d8a8d1cdf61d84c
Zoltan Kiss <kiss.z.zoltan@gmail.com>
2015-09-06 15:52:48 +0200

    Source code grooming.

--- src/main/java/com/kz/jg/cluster/Node.java
+++ src/main/java/com/kz/jg/cluster/Node.java
@@ -1,26 +1,47 @@
 package com.kz.jg.cluster;
 
+import org.apache.log4j.Logger;
 import org.jgroups.JChannel;
 import org.jgroups.blocks.atomic.Counter;
 import org.jgroups.blocks.atomic.CounterService;
 
+/**
+ * Simple cluster member implementation. The cluster displays 'We're started.'
+ * exactly once.
+ * 
+ * @author zoltan-kiss
+ *
+ */
 public class Node {
 
-	/**
-	 * @param args
-	 * @throws Exception
-	 */
-	public static void main(String[] args) throws Exception {
+	// JGroups cluster configuration file.
+	private static final String CLUSTER_CONFIGURATION = "jg.xml";
 
-		JChannel ch = new JChannel("jg.xml");
+	// Name of the cluster.
+	private static final String COUNTER_NAME = "kz-cluster";
 
-		CounterService counterService = new CounterService(ch);
-		ch.connect("counter-cluster");
-		Counter counter = counterService.getOrCreateCounter("counter", 0);
+	// The atomic variable indicating if the message has already been displayed.
+	private static final String ATOMIC_FLAG = "flag";
 
-		if (counter.compareAndSet(0, 1)) {
-			System.out.println("We're started.");
-		} 
+	// The logger.
+	private static final Logger LOGGER = Logger.getLogger(Node.class);
+
+	public static void main(String[] args) {
+
+		try {
+			JChannel ch = new JChannel(CLUSTER_CONFIGURATION);
+
+			CounterService counterService = new CounterService(ch);
+			ch.connect(COUNTER_NAME);
+			Counter counter = counterService.getOrCreateCounter(ATOMIC_FLAG, 0);
+
+			if (counter.compareAndSet(0, 1)) {
+				System.out.println("We're started.");
+			}
+		} catch (Exception e) {
+			LOGGER.error("Unable to establish cluster.", e);
+		}
+
 	}
 
 }
